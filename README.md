2a494a18f670dd5378d71b70fe0919e93e8f1b87
anjali thorat <thorat.anjali06s@gmail.com>
2023-03-30 23:51:05 +0530

    feat added

--- README.md
+++ README.md
@@ -5,10 +5,11 @@ This is a Music Player Web App. Inspired from -
 Traversy Media : https://youtu.be/QTHRWGn_sJw
 
 ## Langauges and Feature Used
-1. HTML
+1. HTML5
 2. CSS
 3. Vanilla JavaScript
 4. CDN JS - Font Awesome 
+5. HTML5 Audio API
 
 #### Note - Here I have used music clips of some songs and their wallpapers for only learning purposes. I am not intended to use them commercially.
 
--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const cover = document.querySelector('#cover');
 const songs = ['AgaNaga','HappyTogether','GOT'];
 
 // Keep Track of Songs
-let songIndex = 0
+let songIndex = 2
 
 // Initially load song info DOm
 loadSong(songs[songIndex])
@@ -40,6 +40,42 @@ function pauseSong(){
     audio.pause()
 }
 
+function prevSong(){
+    songIndex--;
+
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong()
+}
+
+function nextSong(){
+    songIndex++;
+
+    if(songIndex > songs.length - 1 ){
+        songIndex = 0 ;
+    }
+
+    loadSong(songs[songIndex]);
+    playSong()
+
+}
+
+function updateProgress(e){
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    progress.style.width = `${progressPercent}%`
+}
+
+function setProgress(e){
+    const width = this.clientWidth
+    const clickX = e.offsetX
+    const duration = audio.duration
+
+    audio.currentTime = (clickX / width ) * duration
+}
 
 // Event Listeners
 playBtn.addEventListener('click', ()=> {
@@ -52,3 +88,13 @@ playBtn.addEventListener('click', ()=> {
         playSong()
     }
 })
+
+// change song events 
+preBtn.addEventListener('click', prevSong)
+nextBtn.addEventListener('click', nextSong)
+
+audio.addEventListener('timeupdate', updateProgress)
+
+progressContainer.addEventListener('click', setProgress)
+
+audio.addEventListener('ended', nextSong)
